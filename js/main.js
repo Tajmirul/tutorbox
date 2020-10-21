ef6dc2ee0550e2a6ab59a839f0633aa3374280a2
Tajmirul <tasmirolislam@gmail.com>
2020-10-21 13:52:14 +0600

    featuer added

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 $(document).ready(function () {
-
     // slick slider for banner
     $('.slider-type-1').slick({
         dots: true,
         appendDots: $('.slider-dots'),
         arrows: false,
-        // autoplay: true,
+        autoplay: true,
     });
 
     $('.slider-type-2').slick({
         prevArrow: '<i class="arrows prev icofont-rounded-left"></i>',
         nextArrow: '<i class="arrows next icofont-rounded-right"></i>',
         appendArrows: $('.slider-type-2-arrows'),
-        // autoplay: true,
+        autoplay: true,
     });
 
-    // my code ----------------------------------------------------------------------
+    // counter js --------------------------------------------------
+    // $('.counter').counterUp();
 
+
+    // my code ----------------------------------------------------------------------
     // sticky menu ber and go to top button
     $(window).scroll(function () {
         var scrolled = $(window).scrollTop();
@@ -45,4 +47,30 @@ $(document).ready(function () {
         });
     });
 
-});    
+
+    // image resiger --------------------------
+
+    // 1. get width and heigth of first image
+    // var images, heigth, width; 
+
+    // images = $('.tutor-image');
+
+    // function getHeightAndWidth() {
+    //     height = images[0].height;
+    //     width = images[0].width;
+    // };
+
+    // 2. check all images if there height and width match with first one
+    // $.each(images, function(index, item) {
+    //     if ($(item).height !== height) {
+    //         $(item).height = height;
+    //     } else if ($(item).width !== width){
+    //         $(item).width = width;
+    //     }
+    // });
+
+    // 3. if dose not match than change its height and weight as the first one
+
+
+    $('.preloader').fadeOut(400);
+});
